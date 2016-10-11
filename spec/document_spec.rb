f52ebea96c6045a514201b68365b0bebfb58dfbb
Dirichi <dirichi.ikenjoku@alphasights.com>
2016-10-11 17:09:05 +0100

    fix typo

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'byebug'
 
 describe CXML::Document do
   let(:parser) { CXML::Parser.new }
@@ -26,8 +27,9 @@ describe CXML::Document do
   end
 
   describe '#render' do
-    it 'returns and xml result' do
+    it 'returns an xml result' do
       doc = CXML::Document.new(parser.parse(fixture('envelope3.xml')))
+      byebug
       expect { doc.render }.not_to raise_error
     end
   end
