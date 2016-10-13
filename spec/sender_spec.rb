bc4ba941e6afa33217af8db1b56135bd0f63f037
Dirichi <dirichi.ikenjoku@alphasights.com>
2016-10-13 17:13:43 +0100

    fix order in sender field

---
+++ spec/sender_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe CXML::Sender do
+  describe "#render" do
+    it "returns cxml sender field" do
+      builder = CXML.builder
+      sender_credential = CXML::Credential.new
+      sender_credential.domain = 'sender_domain'
+      sender_credential.identity = 'sender_id'
+      sender_credential.shared_secret = "123456"
+
+      sender = described_class.new      
+      sender.user_agent = 'Alphasights LTD'
+      sender.credential = sender_credential
+
+      expect(sender.render(builder).to_xml).to eq(
+'<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE cXML SYSTEM "http://xml.cXML.org/schemas/cXML/1.2.020/InvoiceDetail.dtd">
+<Sender>
+  <Credential domain="sender_domain">
+    <Identity>sender_id</Identity>
+    <SharedSecret>123456</SharedSecret>
+  </Credential>
+  <UserAgent>Alphasights LTD</UserAgent>
+</Sender>
+')
+      
+    end
+  end
+end
