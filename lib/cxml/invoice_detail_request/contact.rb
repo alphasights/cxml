4f5a34b9cc8a2fb63ff102fdd26437551320b6db
Gustavo Puglia <gustavo.puglia@alphasights.com>
2016-10-13 10:24:15 +0100

    Add extrinsic fields to contact

--- lib/cxml/invoice_detail_request/contact.rb
+++ lib/cxml/invoice_detail_request/contact.rb
@@ -1,35 +1,35 @@
 module CXML
   module InvoiceDetailRequest
     class Contact
-      attr_accessor :role, :address_id, :name, :language, :postal_address, :email
+      attr_accessor :role, :name, :language, :postal_address, :email,
+        :primary_study_contact, :case_code, :vatin, :payment_term
 
       def initialize(data={})
         if data.kind_of?(Hash) && !data.empty?
           @role = data['role']
-          @address_id = data['addressID']
           @name = data['Name']
           @email = data['Email']
+          @payment_term = data['PaymentTerm']
+          @primary_study_contact = data['primary_study_contact']
+          @case_code = data['case_code']
+          @vatin = data['vatin']
           @postal_address =
             CXML::InvoiceDetailRequest::PostalAdress.new(data['PostalAddress'])
         end
       end
 
       def render(node)
-        node.Contact(contact_attributes) do |c|
+        node.Contact('role' => role) do |c|
           c.Name(name, 'xm:lang' => 'en')
           c.Email(email, 'name'=> 'default')
+          c.PaymentTerm('payInNumberOfDays' => payment_term)
+          c.Extrinsic(primary_study_contact, 'name' => primary_study_contact) if primary_study_contact
+          c.Extrinsic(case_code, 'name' => case_code) if case_code
+          c.Extrinsic(vatin, 'name' => vatin) if vatin
 
           postal_address.render(c) if postal_address
         end
         node
-      end
-
-      private
-
-      def contact_attributes
-        { 'role' => role }.tap do |ca|
-          ca['addressID'] = address_id if address_id
-        end
       end
     end
   end
