29fd27ef10cfa6dc29d4f3307be4b3574cf55cef
Gustavo Puglia <gustavo.puglia@alphasights.com>
2016-10-17 11:08:38 +0100

    Remove method to instantiate items

--- lib/cxml/invoice_detail_request/order.rb
+++ lib/cxml/invoice_detail_request/order.rb
@@ -6,7 +6,9 @@ module CXML
       def initialize(data={})
         if data.kind_of?(Hash) && !data.empty?
           @payload_id = data[:payload_id]
-          self.items = data[:items]
+          @items = data[:items].map do |args|
+            CXML::InvoiceDetailRequest::ServiceItem.new(args)
+          end
         end
       end
 
@@ -23,14 +25,6 @@ module CXML
           end
         end
         node
-      end
-
-      private
-
-      def items=(items)
-        @items = items.map do|args|
-          CXML::InvoiceDetailRequest::ServiceItem.new(args)
-        end
       end
     end
   end
